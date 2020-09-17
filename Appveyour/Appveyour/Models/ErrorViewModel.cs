c338a83c933bfb076a2db5005acba481447c5c9b
Kugan <kugan@attspace.ventures>
2020-09-17 15:49:28 +0800

    Init

---
+++ Appveyour/Appveyour/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Appveyour.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
